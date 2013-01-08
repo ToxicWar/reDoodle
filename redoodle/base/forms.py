ecfa23d2859708b450887337aa4faab85267edb0
Anton Larkin <toxicwar94@yandex.ru>
2013-01-08 20:40:33 +0400

    + display errors forms + creating rooms + creating chains

--- redoodle/base/forms.py
+++ redoodle/base/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class AddRoomForm(forms.Form):
-    room_name = forms.CharField(label = 'Room name')
+    room_name = forms.CharField(label = 'Room name', max_length=255)
 
 
 class AddChainInRoom(forms.Form):
-    chain_name = forms.CharField(label = 'Chain room')
+    chain_name = forms.CharField(label = 'Chain room', max_length=255)
--- redoodle/base/views.py
+++ redoodle/base/views.py
@@ -1,19 +1,21 @@
 # coding: utf-8
 from django.views.generic.simple import direct_to_template
 from django.shortcuts import redirect
-from redoodle.base.models import Chain
+from redoodle.base.models import Chain, Room
 from redoodle.base.forms import AddRoomForm, AddChainInRoom
 
 
-def index(request):
+def index(request, form=None):
     chainsInDefaultRoom = Chain.objects.filter(room__name='default')
-    form = AddRoomForm()
+    if form == None:
+        form = AddRoomForm()
     return direct_to_template(request, 'reDoodle.html', {'chainsInDefaultRoom': chainsInDefaultRoom, 'form': form})
 
 
-def room(request, room):
+def room(request, room, form=None):
     chainInRoom = Chain.objects.filter(room__name=room)
-    form = AddChainInRoom()
+    if form == None:
+        form = AddChainInRoom()
     return direct_to_template(request, 'room.html', {'room': room, 'chainInRoom': chainInRoom, 'form': form})
 
 
@@ -25,7 +27,10 @@ def add_room(request):
     if request.method == 'POST':
         form = AddRoomForm(request.POST)
         if form.is_valid():
+            r1, r2 = Room.objects.get_or_create(name=form.cleaned_data['room_name'])
             return redirect('/%s/' % form.cleaned_data['room_name'])
+        else:
+            return index(request, form)
     return redirect('index')
 
 
@@ -33,5 +38,9 @@ def add_chain(request):
     if request.method == 'POST':
         form = AddChainInRoom(request.POST)
         if form.is_valid():
+            room = Room.objects.get(name=form.data['room_name'])
+            r1, r2 = room.chain_set.get_or_create(name=form.data['chain_name'], isBlocked=False)
             return redirect('/%s/%s/' % (form.data['room_name'], form.cleaned_data['chain_name']))
+        else:
+            return room(request, form.data['room_name'], form)
     return redirect('/%s/' % form.data['room_name'])
