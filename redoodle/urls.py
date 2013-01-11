a84025d8524fe164fe92b63f1f42fbb9fce752ed
Anton Larkin <toxicwar94@yandex.ru>
2013-01-11 20:25:57 +0400

    + like/dislike

--- redoodle/urls.py
+++ redoodle/urls.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
-from redoodle.base.views import index, room, editor, add_room, add_chain, save_image
+from redoodle.base.views import index, room, editor, add_room, add_chain, save_image, like
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^add_room/$', add_room, name='add_room'),
     url(r'^add_chain/$', add_chain, name='add_chain'),
     url(r'^save_image/$', save_image, name='save_image'),
+    url(r'^like_chain/$', like, name='like_chain'),
     url(r'^(\w+)/$', room, name='room'),
     url(r'^(\w+)/(\w+)/$', editor, name='editor'),
 
