ff72decaec483bdc337dcf2fd18573bac91e873a
ToxicWar <toxicwar94@yandex.ru>
2013-02-17 22:50:15 +0400

    Fix database name

--- redoodle/settings.py
+++ redoodle/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(VIRTUAL_ENV, 'vernel8.db'),  # Or path to database file if using sqlite3.
+        'NAME': os.path.join(VIRTUAL_ENV, 'redoodle.db'),  # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
