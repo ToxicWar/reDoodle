e4d362fefb61c8a9c2b5492a1ab1c927f27cab75
ToxicWar <toxicwar94@yandex.ru>
2013-09-11 22:50:24 +0400

    Add DATABASE_URL

--- site_redoodle/settings/production.py
+++ site_redoodle/settings/production.py
@@ -7,7 +7,7 @@ DATABASES = {}
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -21,7 +21,6 @@ MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 # STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_PATH, 'static'),
