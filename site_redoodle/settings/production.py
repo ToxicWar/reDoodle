70f240b67d84678a5caa347d6386e55ec8f02f44
ToxicWar <toxicwar94@yandex.ru>
2013-09-11 23:58:50 +0400

    Change production settings

--- site_redoodle/settings/production.py
+++ site_redoodle/settings/production.py
@@ -2,20 +2,31 @@
 from .base import *
 import os
 
-DEBUG = False
+DEBUG = True
 DATABASES = {}
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default='postgres://localhost')
+DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = '*'
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 PROJECT_PATH = '/app/'
 
 MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
+# STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
+
+INSTALLED_APPS += (
+    'django_extensions',
+)
