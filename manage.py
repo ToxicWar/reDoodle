c4c9e3072579101a1c2eaeaf1c9f99c65905c9e0
3bl3gamer <3bl3gamer@gmail.com>
2013-10-06 02:22:36 +0400

    The Begining of VK auth

--- manage.py
+++ manage.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "site_redoodle.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "site_redoodle.settings")
+	
+	#import auth.vkauth
+	
+	from django.core.management import execute_from_command_line
+	execute_from_command_line(sys.argv)
