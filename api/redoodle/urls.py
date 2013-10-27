0b4789c99ebe20d32397cc07381d421b1aece6a9
3bl3gamer <3bl3gamer@gmail.com>
2013-10-27 22:47:50 +0400

    My Little Pathfix

--- api/redoodle/urls.py
+++ api/redoodle/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('api.redoodle.views',
     url('^rooms/$', 'room_list', name='ApiRoomList'),
-    url('^room/(?P<pk>[0-9]+)/$', 'room_detail', name='ApiRoomDetail'),
+    url('^rooms/(?P<pk>[0-9]+)/$', 'room_detail', name='ApiRoomDetail'),
     url('^chains/$', 'chain_list', name='ApiChainList'),
-    url('^chain/(?P<pk>[0-9]+)/$', 'chain_detail', name='ApiChainDetail'),
+    url('^chains/(?P<pk>[0-9]+)/$', 'chain_detail', name='ApiChainDetail'),
 )
